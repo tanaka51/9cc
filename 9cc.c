4576ecf5cc9274e29d17bba5937a5da264367d1d
Koichi Tanaka <tanaka51.jp@gmail.com>
2019-04-27 18:37:07 +0900

    可変長ベクタを仕組みだけ導入

--- 9cc.c
+++ 9cc.c
@@ -4,6 +4,31 @@
 #include <stdlib.h>
 #include <string.h>
 
+typedef struct {
+  void **data;
+  int capacity;
+  int len;
+} Vector;
+
+Vector *new_vector() {
+  Vector *vec = malloc(sizeof(Vector));
+
+  vec->data = malloc(sizeof(void *) * 16);
+  vec->capacity = 16;
+  vec->len = 0;
+
+  return vec;
+}
+
+void vec_push(Vector *vec, void *elem) {
+  if (vec->capacity == vec->len) {
+    vec->capacity *= 2;
+    vec->data = realloc(vec->data, sizeof(void *) * vec->capacity);
+  }
+
+  vec->data[vec->len++] = elem;
+}
+
 // トークンの型を表す値
 enum {
   TK_NUM = 256, // 整数トークン
@@ -213,11 +238,48 @@ void gen(Node *node) {
   printf("  push rax\n");
 }
 
+void expect(int line, int expected, int actual) {
+  if (expected == actual) {
+    return;
+  }
+
+  fprintf(stderr, "%d: %d expected, but got %d\n", line, expected, actual);
+
+  exit(1);
+}
+
+void runtest() {
+  Vector *vec = new_vector();
+
+  expect(__LINE__, 0, vec->len);
+
+  for (int i = 0; i < 100; i++) {
+    Token *token = malloc(sizeof(Token));
+    token->ty = TK_NUM;
+    token->val = i;
+
+    vec_push(vec, token);
+  }
+
+  expect(__LINE__, 100, vec->len);
+  expect(__LINE__, 0, ((Token *)vec->data[0])->val);
+  expect(__LINE__, 50, ((Token *)vec->data[50])->val);
+  expect(__LINE__, 99, ((Token *)vec->data[99])->val);
+
+  printf("OK\n");
+}
+
 int main(int argc, char **argv) {
   if (argc != 2) {
     fprintf(stderr, "引数の数が正しくありません\n");
 
     return 1;
+  }
+
+  if (strcmp(argv[1], "-test") == 0) {
+    runtest();
+
+    return 0;
   }
 
   tokenize(argv[1]);
